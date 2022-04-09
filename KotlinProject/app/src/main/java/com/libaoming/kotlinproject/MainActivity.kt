a258b30a5031067086232985c20f472f3d55b6bb
limb <13716572013@163.com>
2022-04-09 11:15:33 +0800

    Test git commit add a log at MainActivity

--- KotlinProject/app/src/main/java/com/libaoming/kotlinproject/MainActivity.kt
+++ KotlinProject/app/src/main/java/com/libaoming/kotlinproject/MainActivity.kt
@@ -2,10 +2,12 @@ package com.libaoming.kotlinproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("limb","测试git")
     }
 }
