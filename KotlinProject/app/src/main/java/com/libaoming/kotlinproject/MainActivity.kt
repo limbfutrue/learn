d1aa56475d4a4a90239c457359ff2c4f86559aca
limb <13716572013@163.com>
2022-04-09 15:54:34 +0800

    删除MainActivity中无用的log日志代码

--- KotlinProject/app/src/main/java/com/libaoming/kotlinproject/MainActivity.kt
+++ KotlinProject/app/src/main/java/com/libaoming/kotlinproject/MainActivity.kt
@@ -8,9 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e("limb","测试git123")
-        Log.e("limb","测试git456")
-        Log.e("limb","测试git789")
-        Log.e("limb","测试git000")
     }
 }
