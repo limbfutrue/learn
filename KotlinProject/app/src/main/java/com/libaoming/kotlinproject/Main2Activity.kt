c725414da8190a73ab3b982bdc883e57fa7c0df7
limbfutrue <13716572013@163.com>
2022-05-21 10:37:32 +0800

    添加log

--- KotlinProject/app/src/main/java/com/libaoming/kotlinproject/Main2Activity.kt
+++ KotlinProject/app/src/main/java/com/libaoming/kotlinproject/Main2Activity.kt
@@ -2,6 +2,7 @@ package com.libaoming.kotlinproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.libaoming.kotlinproject.databinding.ActivityMain2Binding
 
 class Main2Activity : AppCompatActivity() {
@@ -11,5 +12,6 @@ class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        Log.e("TAG", "onCreate: ")
     }
 }
