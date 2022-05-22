514431ea034025cdd748dc456437e56f4b5dd373
limbfutrue <13716572013@163.com>
2022-05-22 17:21:05 +0800

    test2 patch

--- KotlinProject/app/src/main/java/com/libaoming/kotlinproject/sample/navigation/HomeFragment.kt
+++ KotlinProject/app/src/main/java/com/libaoming/kotlinproject/sample/navigation/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.libaoming.kotlinproject.sample.navigation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -22,6 +23,7 @@ class HomeFragment : Fragment() {
         binding.btHomeJump.setOnClickListener {
             it.findNavController().navigate(R.id.otherFragment)
         }
+        Log.e("TAG", "onCreateView: TEST Patch")
         return binding.root
     }
 }
